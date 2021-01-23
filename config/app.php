3e2864ff51e56614837f1a9a3982d930876108ae
abo3adel <abo3adel35@gmail.com>
2021-01-23 15:07:39 +0000

    install required packages

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Weidner\Goutte\GoutteServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Goutte' => Weidner\Goutte\GoutteFacade::class,
     ],
 
 ];
