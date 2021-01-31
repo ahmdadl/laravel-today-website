80c3ba899f8325de5f1cd9d0ed81159fd20a7412
abo3adel <abo3adel35@gmail.com>
2021-01-31 16:08:31 +0000

    add digital-ocean provider scraper

---
+++ app/Scraper/Scrapers/DigitalOcean.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Scraper\Scrapers;
+
+use App\Scraper\AbstractScraper;
+
+final class DigitalOcean extends AbstractScraper
+{
+    protected function extract(): array
+    {
+        return [];
+    }
+}
