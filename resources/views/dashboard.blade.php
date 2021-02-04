cc10d3074dfe77f70afcfde1668ab9d1adebc0c6
abo3adel <abo3adel35@gmail.com>
2021-02-04 22:33:55 +0000

    install laravel backup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,13 @@
-{{-- <x-app-layout> --}}
-    {{-- <x-slot name="header">
-        <h2 class="text-4xl font-semibold leading-tight text-gray-800">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot> --}}
-
-    <div class="py-5">
-        <div class='flex flex-wrap bg-red-600 space-3'>
-            <div class='w-1/3'>
-                {!!Encore\Admin\Controllers\Dashboard::environment()!!}
-            </div>
-            <div class='w-1/3'>
-                {!!Encore\Admin\Controllers\Dashboard::extensions()!!}
-            </div>
-            <div class='w-1/3'>
-                {!!Encore\Admin\Controllers\Dashboard::dependencies()!!}
-            </div>
+<div class="py-5">
+    <div class='flex flex-wrap space-3'>
+        <div class='w-1/3'>
+            {!!Encore\Admin\Controllers\Dashboard::environment()!!}
+        </div>
+        <div class='w-1/3'>
+            {!!Encore\Admin\Controllers\Dashboard::extensions()!!}
+        </div>
+        <div class='w-1/3'>
+            {!!Encore\Admin\Controllers\Dashboard::dependencies()!!}
         </div>
     </div>
-{{-- </x-app-layout> --}}
+</div>
