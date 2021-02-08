02f61a1b3bcf18e09e2ebe9bb64431f39430c439
abo3adel <abo3adel35@gmail.com>
2021-02-08 14:24:52 +0000

    remove unused config files

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\GaugeServiceProvider::class,
 
     ],
 
@@ -227,8 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
-        'Goutte' => Weidner\Goutte\GoutteFacade::class,
 
         'LikeIt' =>  App\Support\Facades\LikeItFacade::class,
     ],
